df724722b5bd3e1a6b01e7638ae2fa236cd58edf
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-01-21 23:15:25 +0200

    created basic pygame class and game class sceleton

--- slitheren.py
+++ slitheren.py
@@ -1,1 +1,26 @@
 import pygame
+import random
+
+class Slither10_game:
+
+
+    def __init__(self,w=640,h=480):
+        self.w = w
+        self.h = h
+        # init display
+       
+        #init gamestate 
+    def play_step(self):
+        pass
+
+if __name__ == '__main__':
+    game = Slither10_game()
+
+    #game loop
+    while True:
+        game.play_step()
+
+        # break if game over
+
+
+    pygame.quit()
