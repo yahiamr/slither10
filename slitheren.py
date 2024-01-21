8888bfe217856fb628beb9cf2abb212543a76d30
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-01-21 23:39:13 +0200

    implemented snake game class with the init function adn food generation

--- slitheren.py
+++ slitheren.py
@@ -1,6 +1,18 @@
 import pygame
 import random
+from enum import Enum 
+from collections import namedtuple
 
+pygame.init()
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+Point = namedtuple('point','x','y')
+BLOCK_SIZE = 20
 class Slither10_game:
 
 
@@ -8,8 +20,28 @@ class Slither10_game:
         self.w = w
         self.h = h
         # init display
-       
-        #init gamestate 
+        self.display = pygame.display.set_mode((self.w,self.h))
+        pygame.display.set_caption('Slither10')
+        self.clock = pygame.time.clock()
+        
+        #init gamestate
+        self.direction =  Direction.RIGHT
+
+        self.head = Point(self.w/2,self.h/2)
+        self.snake = [self.head,
+                      Point(self.head.x-BLOCK_SIZE,self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE),self.head.y)]
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+    def _place_food(self):
+        x = random.randint(0,(self.w-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+        y = random.randint(0,(self.h-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+        self.food = Point(x,y)
+        if self.food in self.snake:
+            self._place_food()
+
     def play_step(self):
         pass
 
